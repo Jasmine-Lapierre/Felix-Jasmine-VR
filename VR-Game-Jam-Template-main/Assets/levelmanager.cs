dbe53e9db7dbae0500ffc982f65e443efcc07a46
Jasmine-Lapierre <2188895@cmontmorency.qc.ca>
2024-04-10 02:33:16 -0400

    ajout four

---
+++ VR-Game-Jam-Template-main/Assets/levelmanager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+
+public class levelmanager : MonoBehaviour
+{
+
+    public GameObject fenetre;
+    public XRKnob roue;
+    private float FramePrecedent;
+    // Start is called before the first frame update
+  
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (roue.value ==1&roue.value != FramePrecedent){
+          GameObject poterie = GameObject.FindWithTag("Poterie");
+            poterie.tag = "Paintable";
+            Debug.Log("Aok AOSKDOPASKDOPASKDOPKASPDKASKDOPAKSDKAOSPDKOASKDASDPOAKSDOPs");
+        }
+        FramePrecedent = roue.value;
+       fenetre.transform.position = new Vector3(10.95f,(roue.value*2.2f),6.05f);
+    }
+}
